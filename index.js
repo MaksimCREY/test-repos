9c1658aa730548c005e0cc228052548ec35b425a
MaksimCREY <maksimctrey@gmail.com>
2023-09-02 22:33:41 +0300

    merger

--- index.js
+++ index.js
@@ -8,10 +8,5 @@ function min(a,b){
     return a-b;
 }
 function umn(a,b){
-    return a*b;
+    return a*b
 }
-function squareSum(numbers){
-    return numbers.reduce(function(sum, n){
-      return (n*n) + sum;
-    }, 0)
-  }
